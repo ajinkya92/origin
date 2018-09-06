5cba0cc5c0ecf6fd8cac34c1217a92f5c15c3508
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-09-06 21:20:14 +0530

    Simple Add New Item Functionality Complete

--- Persist Data Techniques Todoey/FirstViewController.swift
+++ Persist Data Techniques Todoey/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  Persist Data Techniques Todoey
 //
 //  Created by Ajinkya Sonar on 06/09/18.
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var items = ["Go to Udemy", "Add Course to Cart", "Buy The Course"]
 
@@ -17,11 +20,46 @@ class ViewController: UIViewController {
         
         
     }
+    
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        
+        var itemText = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            if let userEnteredText = itemText.text {
+                
+                self.items.append(userEnteredText)
+                
+                self.tableView.reloadData()
+                
+            }
+            
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create New Item"
+            
+            itemText = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
 
     
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension FirstViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
